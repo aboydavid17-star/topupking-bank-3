de52c23d0a971c6efee48fc664537178f738189d
aboydavid17-star <aboydavid17@gmail.com>
2026-05-03 20:22:02 +0100

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "=== STARTING DEBUG ==="
-echo "PORT from Render is: $PORT"
-sed -i "s/80/$PORT/g" /etc/apache2/sites-available/000-default.conf
-sed -i "s/Listen 80/Listen $PORT/g" /etc/apache2/ports.conf
-echo "=== APACHE CONFIG ==="
-cat /etc/apache2/ports.conf
-echo "=== STARTING APACHE ==="
-apache2-foreground
