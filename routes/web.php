1e6ccc7d4d5f1054e7a8ee01f8bf37b61ee527c8
aboydavid17-star <aboydavid17@gmail.com>
2026-05-08 14:19:31 +0100

    Add fund wallet routes and remove temp routes

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\WalletController;
 use Illuminate\Support\Facades\Artisan;
 
-// Public routes
 Route::get('/', function () {
     return redirect()->route('login');
 });
@@ -15,21 +14,20 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 
-// Auth protected routes
 Route::middleware('auth')->group(function () {
     Route::get('/wallet', [WalletController::class, 'index'])->name('wallet');
     Route::get('/buy-data', [WalletController::class, 'buyDataPage'])->name('buy.data');
     Route::post('/buy-data', [WalletController::class, 'buyData']);
+    Route::get('/fund-wallet', [WalletController::class, 'fundWalletPage'])->name('fund.wallet');
+    Route::post('/fund-wallet', [WalletController::class, 'fundWallet']);
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
 
-// Temp route to run migration - DELETE AFTER USE
 Route::get('/run-migrations', function () {
     Artisan::call('migrate', ['--force' => true]);
     return "Migrations done!";
 });
 
-// Temp route for cache clearing - DELETE AFTER USE  
 Route::get('/clear-cache', function() {
     Artisan::call('optimize:clear');
     return 'Cache cleared!';
