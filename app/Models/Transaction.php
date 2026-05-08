b84ab8900add0e865f928648c8c7c87877503a9e
aboydavid17-star <aboydavid17@gmail.com>
2026-05-08 13:22:52 +0100

    Update fillable fields in Transaction model

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,17 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
-        'user_id',
-        'reference', 
-        'amount',
-        'type',
-        'status',
-        'description'
+        'user_id', 'type', 'network', 'phone_number', 'plan_name',
+        'amount', 'balance_before', 'balance_after', 'status',
+        'api_response', 'reference'
     ];
 
     public function user()
