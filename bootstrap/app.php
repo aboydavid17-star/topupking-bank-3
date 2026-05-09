10f47a62378430687aaeed6a30f589f8a1bd0ef6
aboydavid17-star <aboydavid17@gmail.com>
2026-05-09 12:05:50 +0100

    Update trustProxies middleware configuration
    Removed AWS ELB header from trustProxies middleware.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,12 +12,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // FIX: Trust Render proxy to stop redirect loops
-        $middleware->trustProxies(at: '*', headers: Request::HEADER_X_FORWARDED_FOR |
-            Request::HEADER_X_FORWARDED_HOST |
-            Request::HEADER_X_FORWARDED_PORT |
-            Request::HEADER_X_FORWARDED_PROTO |
-            Request::HEADER_X_FORWARDED_AWS_ELB
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR |
+                Request::HEADER_X_FORWARDED_HOST |
+                Request::HEADER_X_FORWARDED_PORT |
+                Request::HEADER_X_FORWARDED_PROTO
         );
     })
     ->withExceptions(function (Exceptions $exceptions) {
