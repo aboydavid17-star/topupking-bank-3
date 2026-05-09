06e472b1b54dfa816828fc8fbba52104f37b7057
aboydavid17-star <aboydavid17@gmail.com>
2026-05-09 12:27:00 +0100

    Add support for AWS ELB headers in request handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,7 +17,8 @@ return Application::configure(basePath: dirname(__DIR__))
             headers: Request::HEADER_X_FORWARDED_FOR |
                 Request::HEADER_X_FORWARDED_HOST |
                 Request::HEADER_X_FORWARDED_PORT |
-                Request::HEADER_X_FORWARDED_PROTO
+                Request::HEADER_X_FORWARDED_PROTO |
+                Request::HEADER_X_FORWARDED_AWS_ELB
         );
     })
     ->withExceptions(function (Exceptions $exceptions) {
