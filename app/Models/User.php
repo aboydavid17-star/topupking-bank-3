6020e742bb1be2cd8538c20ad2d39d010af038cf
aboydavid17-star <aboydavid17@gmail.com>
2026-05-10 13:30:18 +0100

    Add wallet_balance to User model attributes
    Added 'wallet_balance' to fillable attributes and updated casts.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,60 +2,35 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'wallet_balance', // THIS LINE FIXES THE BUG
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'wallet_balance' => 'decimal:2', // ADD THIS TOO
         ];
     }
 
-    /**
-     * Get the wallet for the user.
-     */
-    public function wallet()
-    {
-        return $this->hasOne(Wallet::class);
-    }
-
-    /**
-     * Get the transactions for the user.
-     */
     public function transactions()
     {
         return $this->hasMany(Transaction::class);
