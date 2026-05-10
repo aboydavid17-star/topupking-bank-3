daea8000130d388749b78b13ac638940edcdb5e0
aboydavid17-star <aboydavid17@gmail.com>
2026-05-10 12:32:59 +0100

    Delete routes/web.php

--- routes/web.php
+++
@@ -1,9 +0,0 @@
-use App\Http\Controllers\WalletController;
-
-Route::middleware(['auth'])->group(function () {
-    Route::get('/wallet', [WalletController::class, 'index'])->name('wallet.index');
-    Route::get('/wallet/fund', [WalletController::class, 'showFundingForm'])->name('wallet.fund');
-    Route::post('/wallet/fund', [WalletController::class, 'fund'])->name('fund');
-    Route::get('/wallet/verify', [WalletController::class, 'verifyPayment'])->name('wallet.verify');
-    Route::get('/force-credit', [WalletController::class, 'forceCredit'])->name('wallet.force');
-});
