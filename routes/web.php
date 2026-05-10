2411bb3bcdeb75d6edfa72a502413fc59384d916
aboydavid17-star <aboydavid17@gmail.com>
2026-05-10 12:31:04 +0100

    Refactor wallet fund route naming

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,9 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WalletController;
-
-Route::get('/', function () {
-    return redirect('/wallet');
-});
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/wallet', [WalletController::class, 'index'])->name('wallet.index');
     Route::get('/wallet/fund', [WalletController::class, 'showFundingForm'])->name('wallet.fund');
-    Route::post('/wallet/fund', [WalletController::class, 'fund'])->name('wallet.fund.post');
+    Route::post('/wallet/fund', [WalletController::class, 'fund'])->name('fund');
     Route::get('/wallet/verify', [WalletController::class, 'verifyPayment'])->name('wallet.verify');
     Route::get('/force-credit', [WalletController::class, 'forceCredit'])->name('wallet.force');
 });
-
-require __DIR__.'/auth.php';
