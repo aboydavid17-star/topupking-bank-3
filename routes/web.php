1c5c8d5e437790da84089cc22d690d80a97888f8
aboydavid17-star <aboydavid17@gmail.com>
2026-05-10 03:45:43 +0100

    Add fund routes to wallet functionality

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,6 @@ use App\Http\Controllers\WalletController;
 use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\ProfileController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-*/
-
 Route::get('/', function () {
     return redirect()->route('wallet.index');
 });
@@ -19,13 +13,18 @@ Route::middleware(['auth'])->group(function () {
     
     // Wallet Routes
     Route::get('/wallet', [WalletController::class, 'index'])->name('wallet.index');
+    
+    // Fund Routes - GET for form, POST for processing
+    Route::get('/fund', [WalletController::class, 'showFundForm'])->name('fund.form');
     Route::post('/fund', [WalletController::class, 'fund'])->name('fund');
+    
+    // Paystack Callback
     Route::get('/paystack/callback', [WalletController::class, 'handleGatewayCallback'])->name('paystack.callback');
     
     // Transaction Routes
     Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions');
     
-    // Profile Routes - Breeze default
+    // Profile Routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
