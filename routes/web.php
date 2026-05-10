10dc2b6c3edf69cb03e3358285ad403d15474ea2
aboydavid17-star <aboydavid17@gmail.com>
2026-05-10 17:05:44 +0100

    Update routes to use dashboard view for home

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,24 @@ Route::get('/', function () {
 
 Auth::routes();
 
+// Dashboard
 Route::get('/home', function () {
-    return view('home');
+    return view('dashboard');
 })->middleware('auth')->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware('auth')->name('dashboard');
+
+// Wallet routes your dashboard.blade.php is calling
+Route::get('/wallet', function () {
+    return view('dashboard'); // Just show dashboard for now
+})->middleware('auth')->name('wallet.index');
+
+Route::get('/airtime', function () {
+    return view('dashboard'); 
+})->middleware('auth')->name('airtime');
+
+Route::get('/data', function () {
+    return view('dashboard');
+})->middleware('auth')->name('data');
