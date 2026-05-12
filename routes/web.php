a3972eb52cebee5b665f9764b8b8633542b2dcfd
aboydavid17-star <aboydavid17@gmail.com>
2026-05-12 10:24:28 +0100

    Delete temporary database fix route
    Removed temporary fix route for database adjustments.

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,3 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/fund-wallet', [WalletController::class, 'showFundForm'])->middleware('auth');
 Route::post('/fund-wallet', [WalletController::class, 'fund'])->middleware('auth');
 Route::get('/payment/callback', [WalletController::class, 'handleCallback']);
-
-// TEMPORARY FIX - DELETE AFTER RUNNING ONCE
-Route::get('/fix-topupking-db', function () {
-    DB::statement("ALTER TABLE transactions ALTER COLUMN network DROP NOT NULL");
-    DB::statement("ALTER TABLE transactions ALTER COLUMN phone_number DROP NOT NULL"); 
-    DB::statement("ALTER TABLE transactions ALTER COLUMN plan_name DROP NOT NULL");
-    return "<h1 style='color:green;font-family:Arial;padding:50px;text-align:center;'>DONE: Database fixed. Fund Wallet works now. DELETE THIS ROUTE 👑</h1>";
-});
